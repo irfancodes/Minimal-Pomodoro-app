0dfb9278409dd70e33270d051c53127182fdeec4
irfanukani <irfanukani2@gmail.com>
2022-10-03 18:16:54 +0530

    Basic UI Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Fragment } from "react";
+import Navbar from "./components/Navbar";
+import PomodoroTimer from "./components/PomodoroTimer";
+
 const App = () => {
-  return <div className="bg-gray-900 h-screen w-full">
-    Hello, World!
-  </div>;
+  return (
+    <div className="bg-gray-900 h-screen overflow-y-hidden">
+      <Navbar />
+      <div className="bg-gray-900 w-full grid place-items-center">
+        <PomodoroTimer />
+      </div>
+    </div>
+  );
 };
 export default App;
